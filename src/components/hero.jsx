26444e83c34d5293ad8a90bc5e45bd386a3e27ef
josephinezayawe <josephinezayawe69@gmail.com>
2025-04-15 10:29:40 +0200

    adding description and user profiles

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -31,6 +31,105 @@ const Hero = () => {
                   </i>
                 </h2>
               </div>
+              <h1
+                className="text-4xl md:text-5xl lg:text-6xl xl:7xl font-extrabold tracking-tight
+              text-neutral-900 dark:text-white"
+              >
+                <span className="block mb-1">Design</span>
+                <span
+                  className="bg-clip-text text-transparent bg-gradient-to-r from-amber-500 via-amber-400 
+                to-amber-600"
+                >
+                  Without Limits
+                </span>
+              </h1>
+              <p
+                className="text-base lg:text-lg text-neutral-700 dark:text-neutral-300
+              max-w-2xl font-light"
+              >
+                Elevate your web projects with our streamlined design system
+                powered by the latest Tailwind V4.Create stunning user exprences
+                with state-of-the-art theme switching.
+              </p>
+              <div className="flex gap-3 items-center">
+                <div className="h-px bg-neutral-300 dark:bg-neutral-700 w-12"></div>
+                <p
+                  className="uppercase text-xs text-neutral-500
+                dark:text-neutral-400 font-medium"
+                >
+                  Transition seamlessly
+                </p>
+              </div>
+              <div className="flex flex-wrap gap-3 lg:gap-4">
+                <button
+                  className="px-6 py-3 lg:py-3.5 lg:px-7 rounded-lg
+                bg-neutral-900 hover:bg-black text-white dark:bg-amber-500
+                dark:hover:bg-amber-600 dark:text-neutral-900 font-medium shadow-lg
+                hover:shadow-neutral-900/20 dark:hover:shadow-amber-500/20 flex
+                items-center transition group"
+                >
+                  <i
+                    className="bx bx-code-alt mr-2 lg:mr-3 text-lg lg:text-xl
+                  group-hover:rotate-12 transition-transform"
+                  ></i>
+                  <span>start coding</span>
+                </button>
+                <button
+                  className="px-6 py-3 lg:py-3.5 lg:px-7 rounded-lg
+                bg-neutral-100 hover:bg-neutral-200 dark:hover:bg-neutral-700 dark:bg-neutral-800 text-neutral-900 dark:text-white 
+                font-medium shadow-lg
+                 flex border border-neutral-300 dark:border-neutral-700
+                items-center transition group"
+                >
+                  <i
+                    className="bx bx-book-open mr-2 lg:mr-3 text-lg lg:text-xl
+                  group-hover:opacity-100 opacity-70 transition-opacity"
+                  ></i>
+                  <span>Documentation</span>
+                </button>
+              </div>
+              <div
+                className="flex flex-col sm:flex-row gap-5 sm:items-center
+              pt-6 lg:pt-7 border-t border-neutral-200 dark:border-neutral-800"
+              >
+                <div className="flex -space-x-3">
+                  <img
+                    src="https://randomuser.me/api/portraits/women/21.jpg"
+                    alt="user"
+                    className="w-9 h-9 lg:w-10 lg:h-10 rounded-full
+                    border-2 border-white dark:border-neutral-900 object-cover"
+                  />
+                  <img
+                    src="https://randomuser.me/api/portraits/men/75.jpg"
+                    alt="user"
+                    className="w-9 h-9 lg:w-10 lg:h-10 rounded-full
+                    border-2 border-white dark:border-neutral-900 object-cover"
+                  />
+                  <img
+                    src="https://randomuser.me/api/portraits/women/24.jpg"
+                    alt="user"
+                    className="w-9 h-9 lg:w-10 lg:h-10 rounded-full
+                    border-2 border-white dark:border-neutral-900 object-cover"
+                  />
+                  <span
+                    className="flex items-center justify-center w-9 h-9 lg:w-10
+                  lg:h-10 rounded-full
+                    border-2 border-white dark:border-neutral-900 text-xs font-medium 
+                    bg-neutral-900 dark:bg-amber-500 text-white dark:text-neutral-900"
+                  >
+                    +5k
+                  </span>
+                </div>
+                <div className="space-y-1">
+                  <div className="flex items-center">
+                    <i className="bx bxs-star text-amber-500 mr-1"></i>
+                    <i className="bx bxs-star text-amber-500 mr-1"></i>
+                    <i className="bx bxs-star text-amber-500 mr-1"></i>
+                    <i className="bx bxs-star text-amber-500 mr-1"></i>
+                    <i className="bx bxs-star-half text-amber-500 mr-1"></i>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
